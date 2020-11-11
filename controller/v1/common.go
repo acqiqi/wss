1d61f26dc2dd937c886603e1bd665809cbce1a92
acqiqi <77706579@qq.com>
2020-11-11 19:22:33 +0800

    init project

--- controller/v1/common.go
+++ controller/v1/common.go
@@ -3,8 +3,22 @@ package v1
 import (
 	"github.com/gin-gonic/gin"
 	"wss/lib/e"
+	"wss/lib/setting"
+	"wss/mqtt"
 )
 
 func PushMqttMsg(c *gin.Context) {
+	data := struct {
+		Msg string `json:"msg"`
+		//Users  []receiver_service.UserAdd         `json:"users"`
+	}{}
+	if err := c.BindJSON(&data); err != nil {
+		e.ApiErr(c, err.Error())
+		return
+	}
+
+	mqtt.Publish(setting.MqttSetting.SysPublish+"mdzz", 0x01, data.Msg)
+
 	e.ApiOk(c, "mdzz", e.GetEmptyStruct())
+
 }
